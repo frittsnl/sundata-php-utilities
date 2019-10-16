d982df6e95eabcb24b834b73c32911421e1b66b1
maartenderie <maartenderie@gmail.com>
2019-10-16 17:22:37 +0200

    Better dirs

---
+++ src/FirstUtil/MaartenUtils.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sundata\Utilities\FirstUtil;
+
+class MaartenUtils
+{
+
+  public static function rand(): int
+  {
+    return 4; // https://xkcd.com/221/
+  }
+
+  public static function helloWorld(): void
+  {
+    echo "Hello, World!";
+  }
+
+}
