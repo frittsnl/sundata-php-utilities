4b775ab1122262910ad0b03a5eb13551e5880ee4
maartenderie <maartenderie@gmail.com>
2020-01-06 14:11:32 +0100

    Added ApiRetriever

---
+++ src/Rest/ApiRetriever.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Sundata\Utilities\Rest;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
+use RuntimeException;
+
+abstract class ApiRetriever
+{
+    /** @var string */
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    protected function restGet($url): string
+    {
+        return $this->apiCall('GET', $url);
+    }
+
+    protected function restPost($url, $payload): string
+    {
+        return $this->apiCall('POST', $url, $payload);
+    }
+
+    protected function apiCall(string $verb, $url, $payload = []): string
+    {
+        try {
+            $response = (new Client())->request(
+                $verb,
+                $url,
+                [
+                    'headers' => [],
+                    'exceptions' => false,
+                    'json' => $payload
+                ]
+            );
+
+        } catch (GuzzleException $e) {
+            $this->logAndThrowThirdPartyApiException("Exception reaching $this->name");
+        }
+
+        if ($response->getStatusCode() != 200) {
+            $this->logAndThrowThirdPartyApiException("$this->name returned {$response->getStatusCode()} : {$response->getBody()}");
+        }
+
+        return $response->getBody();
+    }
+
+    private function logAndThrowThirdPartyApiException(string $message)
+    {
+        Log::error("ThirdPartyApiException: $message");
+        throw new RuntimeException($message);
+    }
+}
