54244ea2030504a26342b09f997da8e5f5cf133f
maartenderie <maartenderie@gmail.com>
2020-01-15 11:59:25 +0100

    Fixed logging that couldn't handle arrays

--- src/Rest/ApiRetriever.php
+++ src/Rest/ApiRetriever.php
@@ -11,52 +11,52 @@ use RuntimeException;
 
 abstract class ApiRetriever
 {
-    /** @var string */
-    private $name;
+  /** @var string */
+  private $name;
 
-    public function __construct(string $name)
-    {
-        $this->name = $name;
+  public function __construct(string $name)
+  {
+    $this->name = $name;
+  }
+
+  protected function restGet($url): string
+  {
+    return $this->apiCall('GET', $url);
+  }
+
+  protected function restPost($url, $payload): string
+  {
+    return $this->apiCall('POST', $url, $payload);
+  }
+
+  protected function apiCall(string $verb, $url, $payload = []): string
+  {
+    Log::debug("$verb on $url w payload: " . json_encode($payload));
+    try {
+      $response = (new Client())->request(
+        $verb,
+        $url,
+        [
+          'headers' => [],
+          'exceptions' => false,
+          'json' => $payload
+        ]
+      );
+
+    } catch (GuzzleException $e) {
+      $this->logAndThrowThirdPartyApiException("Exception reaching $this->name");
     }
 
-    protected function restGet($url): string
-    {
-        return $this->apiCall('GET', $url);
+    if ($response->getStatusCode() != 200) {
+      $this->logAndThrowThirdPartyApiException("$this->name returned {$response->getStatusCode()} : {$response->getBody()}");
     }
 
-    protected function restPost($url, $payload): string
-    {
-        return $this->apiCall('POST', $url, $payload);
-    }
+    return $response->getBody();
+  }
 
-    protected function apiCall(string $verb, $url, $payload = []): string
-    {
-      Log::debug("$verb on $url w payload: $payload");
-        try {
-            $response = (new Client())->request(
-                $verb,
-                $url,
-                [
-                    'headers' => [],
-                    'exceptions' => false,
-                    'json' => $payload
-                ]
-            );
-
-        } catch (GuzzleException $e) {
-            $this->logAndThrowThirdPartyApiException("Exception reaching $this->name");
-        }
-
-        if ($response->getStatusCode() != 200) {
-            $this->logAndThrowThirdPartyApiException("$this->name returned {$response->getStatusCode()} : {$response->getBody()}");
-        }
-
-        return $response->getBody();
-    }
-
-    private function logAndThrowThirdPartyApiException(string $message)
-    {
-        Log::error("ThirdPartyApiException: $message");
-        throw new RuntimeException($message);
-    }
+  private function logAndThrowThirdPartyApiException(string $message)
+  {
+    Log::error("ThirdPartyApiException: $message");
+    throw new RuntimeException($message);
+  }
 }
