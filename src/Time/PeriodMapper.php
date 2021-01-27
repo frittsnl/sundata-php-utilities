74f575c5bc660f67bb25befd80b740a019636da7
maartenderie <maartenderie@gmail.com>
2021-01-27 16:58:12 +0100

    Period type for quarter (of an hour)

--- src/Time/PeriodMapper.php
+++ src/Time/PeriodMapper.php
@@ -8,11 +8,12 @@ class PeriodMapper
 {
     const MAP =
         [ // @formatter:off
-            'year'  => ['1y', 365 * 24 * 60 * 60],
-            'month' => ['1m',  30 * 24 * 60 * 60],
-            'week'  => ['1w',   7 * 24 * 60 * 60],
-            'day'   => ['1d',       24 * 60 * 60],
-            'hour'  => ['1h',            60 * 60]
+            'year'  =>      ['1y', 365 * 24 * 60 * 60],
+            'month' =>      ['1M',  30 * 24 * 60 * 60],
+            'week'  =>      ['1w',   7 * 24 * 60 * 60],
+            'day'   =>      ['1d',       24 * 60 * 60],
+            'hour'  =>      ['1h',            60 * 60],
+            'quarter' =>    ['15m',           15 * 60]
         ]; // @formatter:on
 
     public static function getInfluxPeriod($periodType): string
