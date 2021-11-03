e313ca98bbbeb122de08be6230989d9c383e6e63
Mark <markklanser@gmail.com>
2021-11-03 16:28:56 +0100

    Resolve feedback

--- src/Radiation/MeterRadiation.php
+++ src/Radiation/MeterRadiation.php
@@ -2,40 +2,53 @@
 
 namespace Sundata\Utilities\Radiation;
 
+use Carbon\CarbonImmutable;
 use Sundata\Utilities\Sun\SunOrientationAndAngle;
 use Sundata\Utilities\Sun\SunOrientationCalculation;
 
 class MeterRadiation
 {
-    const TIMEZONE = 'CET';
 
-    public function calculateIrradianceForDateTime(MeterRadiationRequest $request) {
-        $sunPos = $this->getSolarPositionForDateTime($request);
-        $irradiance =  $this->calculateSolarIrradiance($sunPos, $request);
+    /**
+     * Calculates the meter specific irradiance in W/m2 based on
+     * the sun's position and global irradiance for a date time
+     *
+     * @return float as W/m2
+     */
+    public static function calculateIrradianceForDateTime(
+        MeterFacts      $facts,
+        CarbonImmutable $dateTime,
+        float           $globalRadiation): float
+    {
+        $sunPos = self::getSolarPositionForDateTime($facts, $dateTime);
+        $irradiance =  self::calculateSolarIrradiance($sunPos, $facts, $globalRadiation);
         return $irradiance < 0 ? 0 : $irradiance;
     }
 
-    public function getSolarPositionForDateTime(MeterRadiationRequest $request): SunOrientationAndAngle {
+    public static function getSolarPositionForDateTime(
+        MeterFacts      $facts,
+        CarbonImmutable $dateTime): SunOrientationAndAngle
+    {
         $sunObject = new SunOrientationCalculation(
-            $request->hour, self::TIMEZONE, $request->lat, $request->long);
+            $dateTime, $dateTime->getTimezone(), $facts->lat, $facts->long);
         return $sunObject->getSunPosition();
     }
 
     /**
      * Using the formula from
      * https://www.pveducation.org/pvcdrom/properties-of-sunlight/arbitrary-orientation-and-tilt
-     *
-     * @param SunOrientationAndAngle $sunPos
-     * @param MeterRadiationRequest $request
-     * @return float|int
      */
-    public function calculateSolarIrradiance(SunOrientationAndAngle $sunPos, MeterRadiationRequest $request) {
+    public static function calculateSolarIrradiance(
+        SunOrientationAndAngle $sunPos,
+        MeterFacts $facts,
+        float $globalRadiation): float
+    {
         $sunAngleRad = deg2rad($sunPos->angle);
         $sunAzimuthRad = deg2rad($sunPos->orientation);
-        $meterAngleRad = deg2rad($request->meterAngle);
-        $meterAzimuthRad = deg2rad($request->meterOrientation);
+        $meterAngleRad = deg2rad($facts->meterAngleInDegrees);
+        $meterAzimuthRad = deg2rad($facts->meterOrientationInDegrees);
 
-        return $request->globalIrradiance *
+        return $globalRadiation *
             (cos($sunAngleRad) * sin($meterAngleRad) * cos($meterAzimuthRad - $sunAzimuthRad)
                 + sin($sunAngleRad) * cos($meterAngleRad));
     }
--- tests/Radiation/MeterRadiationTest.php
+++ tests/Radiation/MeterRadiationTest.php
@@ -2,57 +2,60 @@
 
 namespace Sundata\Utilities\Test\Radiation;
 
+use Carbon\CarbonImmutable;
 use Generator;
 use PHPUnit\Framework\TestCase;
 use Sundata\Utilities\Radiation\MeterRadiation;
-use Sundata\Utilities\Radiation\MeterRadiationRequest;
+use Sundata\Utilities\Radiation\MeterFacts;
 
 class MeterRadiationTest extends TestCase
 {
     public function MeterRadiationDataProvider(): Generator
     {
-        yield 'South facing 80 degrees' => [
-            new MeterRadiationRequest(
-                '52.1009166',
-                '5.36237110',
-                '2021-11-01 12:00:00',
+        yield 'South facing 80 degrees should return 107 W/m2' => [
+            new MeterFacts(
+                52.1009166,
+                5.36237110,
                 80,
-                180,
-                111
+                180
             ),
+            new CarbonImmutable('2021-11-01 11:00:00'),
+            111,
             107.6038497322];
 
-        yield 'East facing 80 degrees' => [
-            new MeterRadiationRequest(
-                '52.1009166',
-                '5.36237110',
-                '2021-11-01 12:00:00',
+        yield 'East facing 80 degrees should return 18 W/m2' => [
+            new MeterFacts(
+                52.1009166,
+                5.36237110,
                 80,
-                90,
-                111
+                90
             ),
+            new CarbonImmutable('2021-11-01 11:00:00'),
+            111,
             18.049368565163];
 
-        yield 'West facing 40 degrees' => [
-            new MeterRadiationRequest(
-                '52.1009166',
-                '5.36237110',
-                '2021-11-01 12:00:00',
+        yield 'West facing 40 degrees should return 26 W/m2' => [
+            new MeterFacts(
+                52.1009166,
+                5.36237110,
                 40,
                 270,
-                111
+
             ),
+            new CarbonImmutable('2021-11-01 11:00:00'),
+            111,
             26.35906520119];
     }
 
     /** @dataProvider MeterRadiationDataProvider */
     public function testItReturnsMeterIrradiance(
-        MeterRadiationRequest $request,
-        $expectedIrradiance
+        MeterFacts $request,
+        CarbonImmutable $dateTime,
+        float $globalIrradiance,
+        float $expectedIrradiance
     )
     {
-        $meterRadiation = new MeterRadiation();
-        $rad = $meterRadiation->calculateIrradianceForDateTime($request);
+        $rad = MeterRadiation::calculateIrradianceForDateTime($request, $dateTime, $globalIrradiance);
         self::assertEquals($expectedIrradiance, $rad);
     }
 }
