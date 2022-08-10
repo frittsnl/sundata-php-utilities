20c134694bb20cd38a24151d2434c630d38eb5a5
maartenderie <maartenderie@gmail.com>
2022-08-10 14:41:23 +0200

    will fail. fixed date offset

--- tests/Sun/SunInfoTest.php
+++ tests/Sun/SunInfoTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 class SunInfoTest extends TestCase
 {
-    function test()
+    function testSunset()
     {
         $lat = 52.0532538;
         $lon = 5.3180957;
@@ -18,14 +18,34 @@ class SunInfoTest extends TestCase
             $lat, $lon, $date
         );
 
-        $this->assertTrue(in_array(
-            $sunInfo->sunset()->toRfc3339String(),
-            ['2022-08-07T21:20:57+02:00', '2022-08-07T21:19:06+02:00']
-        ));
 
-        $this->assertTrue(in_array(
-            $sunInfo->sunrise()->toRfc3339String(),
-            ['2022-08-07T06:08:05+02:00', '2022-08-07T06:09:56+02:00']
-        ));
+        $this->assertEquals(
+            '2022-08-08T21:19:05+02:00',
+            $sunInfo->sunset()->toRfc3339String()
+        );
+
+//        $this->assertTrue(in_array(
+//            $sunInfo->sunset()->toRfc3339String(),
+//            ['2022-08-07T21:20:57+02:00', '2022-08-07T21:19:06+02:00']
+//        ));
+    }
+    function testSunrise()
+    {
+        $lat = 52.0532538;
+        $lon = 5.3180957;
+        $date = Date::of('2022-08-08', 'Europe/Amsterdam');
+
+        $sunInfo = SunInfo::of(
+            $lat, $lon, $date
+        );
+
+        $this->assertEquals(
+            '2022-08-08T06:09:41+02:00',
+            $sunInfo->sunrise()->toRfc3339String()
+        );
+        //        $this->assertTrue(in_array(
+//            $sunInfo->sunrise()->toRfc3339String(),
+//            ['2022-08-07T06:08:05+02:00', '2022-08-07T06:09:56+02:00']
+//        ));
     }
 }
