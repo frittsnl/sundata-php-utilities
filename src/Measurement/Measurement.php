e3d901f0e95c368c69ac3d6ae7cdeb8829b70f4a
maartenderie <maartenderie@gmail.com>
2024-10-31 11:27:17 +0100

    'generic' way of operations for Measurement.php

--- src/Measurement/Measurement.php
+++ src/Measurement/Measurement.php
@@ -2,7 +2,53 @@
 
 namespace Sundata\Utilities\Measurement;
 
-interface Measurement
+use InvalidArgumentException;
+
+abstract class Measurement
 {
-    public function __toString(): string;
+    abstract public function __toString(): string;
+
+    abstract protected function getValue();
+
+    abstract static protected function fromValue($value): self;
+
+    function multiplyBy(Ratio $ratio): self
+    {
+        $newValue = $this->getValue() * $ratio->asDecimalFraction(6);
+        return $this->fromValue($newValue);
+    }
+
+    function multiplyByFloat(float $float): self
+    {
+        return $this->fromValue($this->getValue() * $float);
+    }
+
+    function divideBy(self $other): Ratio
+    {
+        Measurement::assertSameUnits($this, $other);
+        $bigFactor = 1E6;
+        return new Ratio(
+            $bigFactor * $this->getValue(),
+            $bigFactor * $other->getValue()
+        );
+    }
+
+    function subtract(self $other): self
+    {
+        Measurement::assertSameUnits($this, $other);
+        return $this->fromValue($this->getValue() - $other->getValue());
+    }
+
+    function add(self $other): self
+    {
+        Measurement::assertSameUnits($this, $other);
+        return $this->fromValue($this->getValue() + $other->getValue());
+    }
+
+    private static function assertSameUnits(Measurement $param, Measurement $other)
+    {
+        if (get_class($param) !== get_class($other)) {
+            throw new InvalidArgumentException("Can't mix units for operation");
+        }
+    }
 }
